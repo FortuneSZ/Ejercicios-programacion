d2fb2d0070bdc80127049d75c1f2a32ca3a551b2
FortuneSZ <realsSZmusic@gmail.com>
2021-12-26 00:28:45 +0100

    Visual prueba

---
+++ C#/Visual studio/Tema 1/Ejercicio 1 Mostrar suma en pantalla/Ejercicio_01c_01/Program.cs
@@ -0,0 +1,9 @@
+﻿//este programa muestra en pantalla la suma de 118 y 56
+class ejercicio_01c_01
+{
+	static void Main()
+	{
+		System.Console.WriteLine("118+56");
+		System.Console.WriteLine("174");
+	}
+}
---
+++ C#/Visual studio/Tema 1/Ejercicio 2 Realizar operaciones y mostrarlas en pantalla/ejercicio_01c_02/Program.cs
@@ -0,0 +1,13 @@
+﻿/* este programa se trata de realizar diversas operaciones y posteriormente
+mostrarlas por pantalla*/
+class ejercicio_01c_02
+{
+	static void Main()
+	{
+		System.Console.WriteLine(20 - 34);
+
+		System.Console.WriteLine(20 / 3);
+
+		System.Console.WriteLine(28 % 5);
+	}
+}
